da66db76c6d3e32c19d23f0e1870f2f1d051a5e8
Soubhagya1993 <mohapatra.soubhagya@gmail.com>
2022-07-26 03:05:02 +0530

    my own code

---
+++ Day1/BandName.py
@@ -0,0 +1,11 @@
+'''print("hello world")
+
+x = input("whts your name: ")
+print("Hi " + x) '''
+
+print ("Welcome To Band Name Generator.")
+
+x = input("What's Name of the city you grew up in? ")
+y = input("What's your pet name? ")
+
+print ("your Band Name is " + x+"  "+ y)
