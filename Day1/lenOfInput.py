95cad59439a3d82a720c5dd6e450778a0daaf207
Soubhagya1993 <mohapatra.soubhagya@gmail.com>
2022-07-31 02:11:55 +0530

    using length function

--- Day1/lenOfInput.py
+++ Day1/lenOfInput.py
@@ -5,6 +5,6 @@ print( len( input("What is your name: ") ) )
 # using function
 def length():
     x = len(input("What is your name: "))
-    print(x)
-
+    # print("length of name is " + x)   # with this i will get error as TypeError: can only concatenate str (not "int") to str
+    print("length of name is " + str(x))  #using str() to convert int to string
 length()
