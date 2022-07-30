b915804e08797e884995c297fccac34f55c33bdc
Soubhagya1993 <mohapatra.soubhagya@gmail.com>
2022-07-31 02:24:24 +0530

    updated bandname

--- Day1/BandName.py
+++ Day1/BandName.py
@@ -1,14 +1,14 @@
-'''print("hello world")
+#print("hello world")
+#x = input("whts your name: ")
+#print("Hi " + x) 
 
-x = input("whts your name: ")
-print("Hi " + x) '''
 
 print ("Welcome To Band Name Generator.")
 
-'''x = input("What's Name of the city you grew up in?\n")
-y = input("What's your pet name?\n")
+#x = input("What's Name of the city you grew up in?\n")
+#y = input("What's your pet name?\n")
+#print ("your Band Name is " + x+"  "+ y) '''
 
-print ("your Band Name is " + x+"  "+ y) '''
 
 print("Your band name is: " + input("Enter your city of birth: ") + " " +input("what is your pet name: "))
 
