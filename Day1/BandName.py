8b8042232bdc9f1d608be8172cd2ee4de336253a
Soubhagya1993 <mohapatra.soubhagya@gmail.com>
2022-07-31 01:50:18 +0530

    modfification of code to single line

--- Day1/BandName.py
+++ Day1/BandName.py
@@ -5,7 +5,10 @@ print("Hi " + x) '''
 
 print ("Welcome To Band Name Generator.")
 
-x = input("What's Name of the city you grew up in?\n")
+'''x = input("What's Name of the city you grew up in?\n")
 y = input("What's your pet name?\n")
 
-print ("your Band Name is " + x+"  "+ y)
+print ("your Band Name is " + x+"  "+ y) '''
+
+print("Your band name is: " + input("Enter your city of birth: ") + " " +input("what is your pet name: "))
+
